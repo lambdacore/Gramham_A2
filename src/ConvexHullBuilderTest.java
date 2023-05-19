89e4098bbaaa9c700573f3a58e33cce6f76881e9
lambdacore <jonathanwh93@gmail.com>
2023-05-18 21:00:37 -0700

    Fixed tests

--- src/ConvexHullBuilderTest.java
+++ src/ConvexHullBuilderTest.java
@@ -3,12 +3,33 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConvexHullBuilderTest {
 
+    @Test
+    public void testConvexHull_SinglePointInside() {
+        ArrayList<Point2D> points = new ArrayList<>(Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(2000, 2000),
+                new Point2D(3000, 3000),
+                new Point2D(3000, 4000),
+                new Point2D(1000, 3000),
+                new Point2D(1500, 2500) // Point inside the convex hull
+        ));
+        ConvexHullBuilder builder = new ConvexHullBuilder(points);
+        Iterable<Point2D> hull = builder.hull();
+        List<Point2D> expectedHull = Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(2000, 2000),
+                new Point2D(3000, 3000),
+                new Point2D(3000, 4000),
+                new Point2D(1000, 3000)
+        );
+        assertEquals(expectedHull, toList(hull));
+    }
     @Test
     public void testConvexHull() {
         // Create an ArrayList of points for testing
@@ -24,25 +45,110 @@ public class ConvexHullBuilderTest {
         ConvexHullBuilder builder = new ConvexHullBuilder(points);
         Iterable<Point2D> hull = builder.hull();
 
-        // Convert the expected result to an ArrayList for easier comparison
-        ArrayList<Point2D> expectedHull = new ArrayList<>(Arrays.asList(
+        // Convert the expected result to a List for easier comparison
+        List<Point2D> expectedHull = Arrays.asList(
                 new Point2D(1000, 2000),
                 new Point2D(2000, 2000),
-                new Point2D(1000, 3000),
                 new Point2D(3000, 3000),
-                new Point2D(3000, 4000)
-        ));
-
-        // Sort the expected hull points lexicographically for consistent comparison
-        Collections.sort(expectedHull);
+                new Point2D(3000, 4000),
+                new Point2D(1000, 3000)
+        );
 
         // Compare the expected hull and the actual hull
-        assertEquals(expectedHull, toArrayList(hull));
+        assertEquals(expectedHull, toList(hull));
+    }
+    @Test
+    public void testConvexHull_Square() {
+        ArrayList<Point2D> points = new ArrayList<>(Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(2000, 2000),
+                new Point2D(2000, 3000),
+                new Point2D(1000, 3000)
+        ));
+
+        ConvexHullBuilder builder = new ConvexHullBuilder(points);
+        Iterable<Point2D> hull = builder.hull();
+
+        List<Point2D> expectedHull = Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(2000, 2000),
+                new Point2D(2000, 3000),
+                new Point2D(1000, 3000)
+        );
+
+        assertEquals(expectedHull, toList(hull));
     }
 
-    // Helper method to convert an Iterable<Point2D> to an ArrayList<Point2D>
-    private ArrayList<Point2D> toArrayList(Iterable<Point2D> iterable) {
-        ArrayList<Point2D> list = new ArrayList<>();
+    @Test
+    public void testConvexHull_CollinearPoints() {
+        ArrayList<Point2D> points = new ArrayList<>(Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(2000, 2000),
+                new Point2D(3000, 2000),
+                new Point2D(4000, 2000)
+        ));
+
+        ConvexHullBuilder builder = new ConvexHullBuilder(points);
+        Iterable<Point2D> hull = builder.hull();
+
+        List<Point2D> expectedHull = Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(4000, 2000)
+        );
+
+        assertEquals(expectedHull, toList(hull));
+    }
+
+    @Test
+    public void testConvexHull_ThreePoints() {
+        ArrayList<Point2D> points = new ArrayList<>(Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(2000, 1000),
+                new Point2D(1500, 3000)
+        ));
+
+        ConvexHullBuilder builder = new ConvexHullBuilder(points);
+        Iterable<Point2D> hull = builder.hull();
+
+        List<Point2D> expectedHull = Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(2000, 2000),
+                new Point2D(1500, 3000)
+        );
+
+        assertEquals(expectedHull, toList(hull));
+    }
+
+    @Test
+    public void testConvexHullWithCollinearPoints() {
+        // Create an ArrayList of collinear points for testing
+        ArrayList<Point2D> points = new ArrayList<>(Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(2000, 2000),
+                new Point2D(3000, 2000),
+                new Point2D(4000, 2000),
+                new Point2D(5000, 2000),
+                new Point2D(6000, 2000)
+        ));
+
+        ConvexHullBuilder builder = new ConvexHullBuilder(points);
+        Iterable<Point2D> hull = builder.hull();
+
+        // The convex hull of collinear points should be the endpoints only
+        List<Point2D> expectedHull = Arrays.asList(
+                new Point2D(1000, 2000),
+                new Point2D(6000, 2000)
+        );
+
+        // Compare the expected hull and the actual hull
+        assertEquals(expectedHull, toList(hull));
+    }
+
+    // Add more test cases as needed
+
+    // Helper method to convert an Iterable<Point2D> to a List<Point2D>
+    private List<Point2D> toList(Iterable<Point2D> iterable) {
+        List<Point2D> list = new ArrayList<>();
         for (Point2D point : iterable) {
             list.add(point);
         }
